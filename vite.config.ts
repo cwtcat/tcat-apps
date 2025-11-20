f84f852b7eef7330446c9b1d2d958767a1e1e36c
Chris Westling <cmwtcat@gmail.com>
2025-11-20 15:39:12 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Determine base path:
+// - In development, Vite needs "/"  (served at http://localhost:5173/)
+// - In production, we serve the app under "/apps/ridership/"
+const isProd = process.env.NODE_ENV === 'production'
+const base = isProd ? '/apps/ridership/' : '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/apps/ridership/',
-}) 
+  base,
+})
